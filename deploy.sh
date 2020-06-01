0559bb034b8977acaff6656957a34fc5a847ab08
harishchitipothu <57835771+harishchitipothu@users.noreply.github.com>
2020-06-01 13:15:59 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 
 echo "Deploying to $env"
 echo "_______________________________"
-serverless config credentials --provider aws --key AKIAIE6LCOJQH6NERRDQ --secret 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
+serverless config credentials --provider aws --key AKIAJJNVBL3PWNZYN3EQ --secret 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
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
